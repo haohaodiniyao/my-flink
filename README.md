f4822d44020ddb4d78b5e1b8ede8edb744802257
yaokai <yaokai@ushareit.com>
2022-02-02 22:06:24 +0800

    redis

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # my flink
 https://nightlies.apache.org/flink/flink-docs-release-1.12/zh/dev/connectors/jdbc.html
+
+https://bahir.apache.org/docs/flink/1.0/flink-streaming-redis/
+
+从socket获取数据
+nc -lk 9999
---
+++ src/main/java/com/example/RedisDemo.java
@@ -0,0 +1,56 @@
+package com.example;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.redis.RedisSink;
+import org.apache.flink.streaming.connectors.redis.common.config.FlinkJedisPoolConfig;
+import org.apache.flink.streaming.connectors.redis.common.mapper.RedisCommand;
+import org.apache.flink.streaming.connectors.redis.common.mapper.RedisCommandDescription;
+import org.apache.flink.streaming.connectors.redis.common.mapper.RedisMapper;
+import org.apache.flink.util.Collector;
+
+/**
+ * 数据写入redis
+ * https://bahir.apache.org/docs/flink/1.0/flink-streaming-redis/
+ */
+public class RedisDemo {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        DataStream<String> lines = env.socketTextStream("localhost", 9999);
+
+        SingleOutputStreamOperator<Tuple2<String, Integer>> sum = lines.flatMap(new LineSplitter()).keyBy(t -> t.f0).sum(1);
+        FlinkJedisPoolConfig conf = new FlinkJedisPoolConfig.Builder().setHost("localhost").setPort(55008).build();
+        sum.addSink(new RedisSink<>(conf,new RedisExampleMapper()));
+        env.execute();
+    }
+
+    public static class LineSplitter implements FlatMapFunction<String, Tuple2<String, Integer>> {
+        @Override
+        public void flatMap(String line, Collector<Tuple2<String, Integer>> out) {
+            for (String word : line.split(" ")) {
+                out.collect(new Tuple2<String, Integer>(word, 1));
+            }
+        }
+    }
+
+    public static class RedisExampleMapper implements RedisMapper<Tuple2<String, Integer>> {
+
+        @Override
+        public RedisCommandDescription getCommandDescription() {
+            return new RedisCommandDescription(RedisCommand.HSET, "wc_result");
+        }
+
+        @Override
+        public String getKeyFromData(Tuple2<String, Integer> data) {
+            return data.f0;
+        }
+
+        @Override
+        public String getValueFromData(Tuple2<String, Integer> data) {
+            return data.f1.toString();
+        }
+    }
+}
